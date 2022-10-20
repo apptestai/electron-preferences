8963019931d9c4939bc1c60fdbd949a6dbcda244
이대희 <idh.lee@apptest.ai>
2022-10-20 12:43:37 +0900

    debug

--- src/app/components/main/components/group/components/fields/button/index.jsx
+++ src/app/components/main/components/group/components/fields/button/index.jsx
@@ -1,57 +1,49 @@
 'use strict';
 
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
 class ButtonField extends React.Component {
+  render() {
+    console.log('button');
+    return (
+      <div className="field field-button">
+        <input
+          type="button"
+          onClick={this.onClick.bind(this)}
+          value={this.label}
+          aria-label={this.label}
+        />
+        {this.help && <span className="help">{this.help}</span>}
+      </div>
+    );
+  }
 
-	render() {
+  get field() {
+    return this.props.field;
+  }
 
-		return (
-			<div className="field field-button">
-				<input type="button" onClick={ this.onClick.bind(this) } value={ this.label } aria-label={ this.label }/>
-				{ this.help && <span className="help">{ this.help }</span> }
-			</div>
-		);
+  get value() {
+    return this.field.label;
+  }
 
-	}
+  get label() {
+    return this.field.label;
+  }
 
-	get field() {
+  get help() {
+    return this.field.help;
+  }
 
-		return this.props.field;
-
-	}
-
-	get value() {
-
-		return this.field.label;
-
-	}
-
-	get label() {
-
-		return this.field.label;
-
-	}
-
-	get help() {
-
-		return this.field.help;
-
-	}
-
-	onClick() {
-
-		return this.props.onChange(true);
-
-	}
-
+  onClick() {
+    return this.props.onChange(true);
+  }
 }
 
 ButtonField.propTypes = {
-	field: PropTypes.object,
-	value: PropTypes.string,
-	onChange: PropTypes.func,
+  field: PropTypes.object,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 export default ButtonField;
--- src/app/components/main/components/group/components/fields/checkbox/index.jsx
+++ src/app/components/main/components/group/components/fields/checkbox/index.jsx
@@ -1,131 +1,107 @@
 'use strict';
 
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import { newGuid } from '../../../../../../../utils/newGuid';
 
 class CheckboxField extends React.Component {
+  render() {
+    let { value } = this;
+    const { help, label } = this;
 
-	render() {
+    const fieldID = `checkbox_${newGuid()}`;
 
-		let { value } = this;
-		const { help, label } = this;
+    const options = this.options.map((option, idx) => {
+      console.log(option, '!!!');
+      // If only a single checkbox is being rendered, this allows you the ability to pass
+      // a boolean default value instead of ['value'], for convenience.
+      if (typeof value === 'boolean' && this.options.length === 1) {
+        value = value ? [option.value] : [];
+      } else if (typeof value !== 'object') {
+        value = [];
+      }
 
-		const fieldID = `checkbox_${newGuid()}`;
+      const id = `${fieldID}_${idx}`;
+      const checked = value.indexOf(option.value) >= 0;
 
-		const options = this.options.map((option, idx) => {
+      return (
+        <label htmlFor={id} className="checkbox-option" key={idx}>
+          {option.label}
+          <input
+            type="checkbox"
+            id={id}
+            onChange={this.onChange.bind(this)}
+            checked={checked}
+            aria-label={option.label}
+            disabled={option.disabled ? 'disabled' : ''}
+          />
+          <span className="check-square" />
+        </label>
+      );
+    });
 
-			// If only a single checkbox is being rendered, this allows you the ability to pass
-			// a boolean default value instead of ['value'], for convenience.
-			if (typeof value === 'boolean' && this.options.length === 1) {
+    return (
+      <div className="field field-checkbox">
+        <div className="field-label" aria-label={label}>
+          {label}
+        </div>
+        {options}
+        {help && <span className="help">{help}</span>}
+      </div>
+    );
+  }
 
-				value = value ? [ option.value ] : [];
+  get field() {
+    return this.props.field;
+  }
 
-			} else if (typeof value !== 'object') {
+  get value() {
+    return this.props.value || [];
+  }
 
-				value = [];
+  get label() {
+    return this.field.label;
+  }
 
-			}
+  get options() {
+    return this.field.options || [];
+  }
 
-			const id = `${fieldID}_${idx}`;
-			const checked = value.indexOf(option.value) >= 0;
+  get help() {
+    return this.field.help;
+  }
 
-			return (
-				<label htmlFor={ id } className="checkbox-option" key={idx}>
-					{ option.label }
-					<input type="checkbox" id={ id } onChange={ this.onChange.bind(this) } checked={ checked } aria-label={ option.label } />
-					<span className="check-square" />
-				</label>
-			);
+  onChange(e) {
+    let { value } = this;
+    const idx = e.target.id.split('_')[2];
+    const option = this.options[idx];
 
-		});
+    // Coerce values
+    if (typeof value === 'boolean' && this.options.length === 1) {
+      value = value ? [option.value] : [];
+    } else if (typeof value !== 'object') {
+      value = [];
+    }
 
-		return (
-			<div className="field field-checkbox">
-				<div className="field-label" aria-label={ label }>{ label }</div>
-				{ options }
-				{ help && <span className="help">{ help }</span> }
-			</div>
-		);
+    if (e.target.checked) {
+      if (value.indexOf(option.value) === -1) {
+        value.push(option.value);
+      }
+    } else {
+      const valIdx = value.indexOf(option.value);
+      if (valIdx > -1) {
+        value.splice(valIdx, 1);
+      }
+    }
 
-	}
-
-	get field() {
-
-		return this.props.field;
-
-	}
-
-	get value() {
-
-		return this.props.value || [];
-
-	}
-
-	get label() {
-
-		return this.field.label;
-
-	}
-
-	get options() {
-
-		return this.field.options || [];
-
-	}
-
-	get help() {
-
-		return this.field.help;
-
-	}
-
-	onChange(e) {
-
-		let { value } = this;
-		const idx = e.target.id.split('_')[2];
-		const option = this.options[idx];
-
-		// Coerce values
-		if (typeof value === 'boolean' && this.options.length === 1) {
-
-			value = value ? [ option.value ] : [];
-
-		} else if (typeof value !== 'object') {
-
-			value = [];
-
-		}
-
-		if (e.target.checked) {
-
-			if (value.indexOf(option.value) === -1) {
-
-				value.push(option.value);
-
-			}
-
-		} else {
-
-			const valIdx = value.indexOf(option.value);
-			if (valIdx > -1) {
-
-				value.splice(valIdx, 1);
-
-			}
-
-		}
-
-		return this.props.onChange(value);
-
-	}
-
+    return this.props.onChange(value);
+  }
 }
 
 CheckboxField.propTypes = {
-	field: PropTypes.object,
-	value: PropTypes.array,
-	onChange: PropTypes.func,
+  field: PropTypes.object,
+  value: PropTypes.array,
+  onChange: PropTypes.func,
 };
 
 export default CheckboxField;
